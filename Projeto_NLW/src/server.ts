6e3d451468cea4b22a79415efc9a5ccc47080892
Nilson R S Vieira <nilsonrsvieira@gmail.com>
2021-04-19 23:36:40 -0300

    Primeiro Projeto NodeJS com Typescript

---
+++ Projeto_NLW/src/server.ts
@@ -0,0 +1,24 @@
+import express, { request, response } from "express";
+
+const app = express();
+
+/**
+ * GET = Buscas
+ * POST = Criação
+ * PUT = Alteração
+ * DELETE = Deletar
+ * PATCH = Alterar uma informação específica
+ */
+app.get("/", (request, response) => {
+  return response.json({
+    "message": "Olá NLW05"
+  });
+});
+
+app.post("/users", (request, response) => {
+  return response.json({
+    "message": "Usuário Salvo com Sucesso!"
+  });
+});
+
+app.listen(3333, () => console.log("Server is running on port 3333"));
